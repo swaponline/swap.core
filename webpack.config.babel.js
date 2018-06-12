47591c4c02d84cb8e564dc2f0f67a75952a56e32
Pavel Ivanov <grammka@gmail.com>
2018-06-12 11:02:41 +0300

    try add vanilla example

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,19 +2,31 @@ import webpack from 'webpack'
 import path from 'path'
 
 
+const resolveSrcPath = (filePath) => path.resolve(__dirname, `./src/${filePath}`)
+
 const globals = {
   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 }
 
 const webpackConfig = {
-  mode: process.env.NODE_ENV,
-  entry: {},
+
+  mode: 'production',
+
+  entry: {
+    'auth': resolveSrcPath('swap.auth/umd.js'),
+    'orders': resolveSrcPath('swap.orders/umd.js'),
+    'room': resolveSrcPath('swap.room/umd.js'),
+    'app': resolveSrcPath('swap.app/umd.js'),
+    'flows': resolveSrcPath('swap.flows/umd.js'),
+    'swap': resolveSrcPath('swap.swap/umd.js'),
+    'swaps': resolveSrcPath('swap.swaps/umd.js'),
+  },
 
   output: {
-    path: path.join(__dirname, 'example'),
-    filename: 'swap-core.js',
+    path: path.join(__dirname, 'umd'),
+    filename: 'swap.[name].js',
     libraryTarget: 'umd',
-    library: 'swapCore',
+    library: [ 'swap', '[name]' ],
   },
 
   module: {
@@ -27,9 +39,30 @@ const webpackConfig = {
     ]
   },
 
+  // resolve: {
+  //   alias: {
+  //     'swap.auth': resolveSrcPath('swap.auth'),
+  //     'swap.orders': resolveSrcPath('swap.orders'),
+  //     'swap.room': resolveSrcPath('swap.room'),
+  //     'swap.app': resolveSrcPath('swap.app'),
+  //     'swap.flows': resolveSrcPath('swap.flows'),
+  //     'swap.swap': resolveSrcPath('swap.swap'),
+  //     'swap.swaps': resolveSrcPath('swap.swaps'),
+  //   },
+  // },
+
   plugins: [
     new webpack.DefinePlugin(globals),
-  ]
+    new webpack.ProvidePlugin({
+      'swap.auth': 'swap.auth',
+      'swap.orders': 'swap.orders',
+      'swap.room': 'swap.room',
+      'swap.app': 'swap.app',
+      'swap.flows': 'swap.flows',
+      'swap.swap': 'swap.swap',
+      'swap.swaps': 'swap.swaps',
+    }),
+  ],
 }
 
 
