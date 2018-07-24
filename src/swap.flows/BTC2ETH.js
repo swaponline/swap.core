92cd868a8ec3a9137abba641b8e4e51ad31be041
Aleksey Bykhun <theisease@gmail.com>
2018-07-24 17:47:18 +0300

    * better error message for continious balance check

--- src/swap.flows/BTC2ETH.js
+++ src/swap.flows/BTC2ETH.js
@@ -195,7 +195,7 @@ class BTC2ETH extends Flow {
         })
 
         if (balanceCheckResult) {
-          console.error(`Eth balance check error:`, balanceCheckResult)
+          console.error(`Waiting until deposit: ETH balance check error:`, balanceCheckResult)
           flow.swap.events.dispatch('eth balance check error', balanceCheckResult)
           return
         }
