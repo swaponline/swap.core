d2160fe8700c2a8c15e07c4a90f3827ee2dc749f
Aleksey Bykhun <caffeinum@Alekseys-MacBook-Pro.local>
2018-08-23 17:49:40 +0300

    fix error from fix/set-participant

--- src/swap.swap/Swap.js
+++ src/swap.swap/Swap.js
@@ -72,7 +72,8 @@ class Swap {
     if (!swap.participant && !isMy) {
       console.error(`swap.participant should be set after order request, please check current state: something's wrong.`)
       console.log(swap.owner, swap.participant)
-      swap.participant = SwapApp.services.auth.getPublicData()
+      swap.participant = swap.owner
+      SwapApp.services.auth.getPublicData()
     }
 
     return swap
