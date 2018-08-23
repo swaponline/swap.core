ae959017d30dc7a56109c9f4329ff43711bff4d6
Aleksey Bykhun <caffeinum@Alekseys-MacBook-Pro.local>
2018-08-23 18:32:27 +0300

    remove request lib

--- src/swap.swaps/usdt/unspents.js
+++
@@ -1,8 +0,0 @@
-const { fetchUnspents } = require('./api')
-
-module.exports = async (address) => {
-  const unspents = await fetchUnspents(address)
-  const totalUnspent  = unspents.reduce((summ, { satoshis }) => summ + satoshis, 0)
-
-  return { totalUnspent, unspents }
-}
