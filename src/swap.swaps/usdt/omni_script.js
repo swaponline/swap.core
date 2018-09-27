792b9837011a1e0c4e24e6e7dc02d74ad5b2c9b7
Aleksey Bykhun <theisease@gmail.com>
2018-09-27 13:41:51 +0300

    usdt: fix non-integer sends

--- src/swap.swaps/usdt/omni_script.js
+++ src/swap.swaps/usdt/omni_script.js
@@ -9,11 +9,12 @@ module.exports = (amount, coin = 31) => {
 
   const simple_send = [
     "6f6d6e69", // omni
+    "0000",     // tx type
     "0000",     // version
-    // "00000000001f", // 31 for Tether
-    toPaddedHexString(coin, 12),
+    // "0000001f", // 31 for Tether
+    toPaddedHexString(coin, 8),
     // "000000003B9ACA00" // amount = 10 * 100 000 000 in HEX
-    toPaddedHexString(amount * 100000000, 16),
+    toPaddedHexString(Math.floor(amount * 100000000), 16),
   ].join('')
 
   const data = Buffer.from(simple_send, "hex")
