601db3cd47722b11b954c7cd1e986fddaed1acca
Aleksey Bykhun <theisease@gmail.com>
2019-01-09 22:24:37 +0300

    simple: load swap.core from local dir, if available

--- simple/src/index.js
+++ simple/src/index.js
@@ -3,6 +3,8 @@ require('babel-core/register')
 
 require('app-module-path/register') // on windows
 
+require('app-module-path').addPath(__dirname + '/../../src')
+require('app-module-path').addPath(__dirname + '/../../lib')
 require('app-module-path').addPath(__dirname + '/../node_modules/swap.core/lib')
 require('app-module-path').addPath(__dirname + '/../swap.core/lib')
 require('app-module-path').addPath(__dirname + '/../../swap.core/lib')
