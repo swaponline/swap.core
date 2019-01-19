9b217ee7df3098d70bd36a9602ded2c115b1f808
Aleksey Bykhun <theisease@gmail.com>
2019-01-19 05:41:15 +0300

    btcSwap: if no expected.confidence given, use 0.99 as OK level

--- src/swap.swaps/BtcSwap.js
+++ src/swap.swaps/BtcSwap.js
@@ -216,7 +216,7 @@ class BtcSwap extends SwapInterface {
     const { recipientPublicKey, lockTime } = data
     const { scriptAddress, script } = this.createScript(data, hashName)
 
-    const expectedConfidence = expected.confidence
+    const expectedConfidence = expected.confidence || 0.99
     const unspents      = await this.fetchUnspents(scriptAddress)
     const expectedValue = expected.value.multipliedBy(1e8).integerValue()
     const totalUnspent  = unspents.reduce((summ, { satoshis }) => summ + satoshis, 0)
