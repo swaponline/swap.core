09e273fdecfc877420d3dc58e9f8a6cf461fbf66
Aleksey Bykhun <theisease@gmail.com>
2019-01-30 15:09:03 +0300

    simple: update core import

--- simple/src/index.js
+++ simple/src/index.js
@@ -5,14 +5,17 @@ require('app-module-path/register') // on windows
 
 require('app-module-path').addPath(__dirname + '/../../src')
 require('app-module-path').addPath(__dirname + '/../../lib')
+require('app-module-path').addPath(__dirname + '/../node_modules/swap.core/src')
 require('app-module-path').addPath(__dirname + '/../node_modules/swap.core/lib')
+require('app-module-path').addPath(__dirname + '/../swap.core/src')
 require('app-module-path').addPath(__dirname + '/../swap.core/lib')
+require('app-module-path').addPath(__dirname + '/../../swap.core/src')
 require('app-module-path').addPath(__dirname + '/../../swap.core/lib')
 
 const setup = require('./setup')
 const helpers = require('./helpers')
 const config = require('./config')
 
-import { constants } from 'swap.app'
+const { constants } = require('swap.core')
 
 module.exports = { setup, helpers, config, constants }
