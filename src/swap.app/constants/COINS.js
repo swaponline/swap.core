03176155a4d7f4f9d490006be73a21592b2505b2
Pasha8914 <uhau-buhau@mail.ru>
2019-07-27 13:40:17 +1000

    prettier ^_^

--- src/swap.app/constants/COINS.js
+++ src/swap.app/constants/COINS.js
@@ -1,25 +1,25 @@
 export const NATIVE = {
-  eth: "ETH",
-  btc: "BTC",
-  bch: "BCH",
-  ltc: "LTC",
-  eos: "EOS",
-  nim: "NIM",
-  usdt: "USDT"
-};
+  eth: 'ETH',
+  btc: 'BTC',
+  bch: 'BCH',
+  ltc: 'LTC',
+  eos: 'EOS',
+  nim: 'NIM',
+  usdt: 'USDT',
+}
 
 export const ETH_TOKENS = {
-  noxon: "NOXON",
-  swap: "SWAP",
-  pbl: "PBL",
-  xsat: "XSAT",
-  hdp: "HDP",
-  scro: "SCRO",
-  xeur: "XEUR",
-  eurs: "EURS"
-};
+  noxon: 'NOXON',
+  swap: 'SWAP',
+  pbl: 'PBL',
+  xsat: 'XSAT',
+  hdp: 'HDP',
+  scro: 'SCRO',
+  xeur: 'XEUR',
+  eurs: 'EURS',
+}
 
 export default {
   ...NATIVE,
-  ...ETH_TOKENS
-};
+  ...ETH_TOKENS,
+}
