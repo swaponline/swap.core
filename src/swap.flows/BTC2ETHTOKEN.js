712dc7bb2f9ce04a3e33e794ab0631ae3fc1c692
shendel <im_shendel@inbox.ru>
2019-08-02 12:56:30 +0300

    case-insensitive destination

--- src/swap.flows/BTC2ETHTOKEN.js
+++ src/swap.flows/BTC2ETHTOKEN.js
@@ -311,7 +311,7 @@ export default (tokenName) => {
               ? flow.swap.destinationBuyAddress
               : this.app.services.auth.accounts.eth.address
 
-            if (targetWallet != needTargetWallet) {
+            if (targetWallet.toLowerCase() != needTargetWallet.toLowerCase()) {
               console.error(
                 "Destination address for tokens dismatch with needed (Needed, Getted). Stop swap now!",
                 needTargetWallet,
