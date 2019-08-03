c65787c2bc32e1f384e200d34f52278787492f65
Pavel Ivanov <grammka@gmail.com>
2019-08-03 15:52:32 +0300

    add QTUM coin to constants

--- src/swap.app/util/typeforce.js
+++ src/swap.app/util/typeforce.js
@@ -22,6 +22,7 @@ const isCoinAddress = {
   [constants.COINS.btc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{26,35}$/.test(value),
   [constants.COINS.bch]: (value) => typeof value === 'string' && /^[A-Za-z0-9:]{26,54}$/.test(value),
   [constants.COINS.ltc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{34}$/.test(value),
+  [constants.COINS.qtum]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{34}$/.test(value),
   // [constants.COINS.usdt]: (value) => typeof value === 'string',
 }
 
@@ -31,6 +32,7 @@ const isPublicKey = {
   [constants.COINS.btc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
   [constants.COINS.bch]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
   [constants.COINS.ltc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
+  [constants.COINS.qtum]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
   // [constants.COINS.usdt]: '?String', // TODO we don't have / use nim publicKey
 }
 
