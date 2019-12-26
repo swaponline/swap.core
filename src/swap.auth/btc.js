16f4d35211105a479092ce65ff283135ae694554
nthgness <zalunin.alexey@gmail.com>
2019-12-26 15:50:44 +0700

    fix methods in login

--- src/swap.auth/btc.js
+++ src/swap.auth/btc.js
@@ -6,6 +6,7 @@ const login = (_privateKey, app) => {
 
   const storageKey = `${app.network}:btc:privateKey`
   let privateKey = _privateKey || app.env.storage.getItem(storageKey)
+  
   let account
 
   const network = (
@@ -20,15 +21,21 @@ const login = (_privateKey, app) => {
 
   account = new app.env.bitcoin.ECPair.fromWIF(privateKey, network)
 
-  account.getPublicKey = () => account.getPublicKeyBuffer().toString('hex')
+  const { address } = app.env.bitcoin.payments.p2pkh({ pubkey: account.publicKey, network })
+  const { publicKey } = account
+
+  account.getPublicKey = () => publicKey.toString('hex')
   account.getPrivateKey = () => privateKey
+  account.getAddress = () => address
 
   if (!_privateKey) {
     app.env.storage.setItem(storageKey, privateKey)
   }
 
+
   return account
 }
+
 
 const getPublicData = (account) => ({
   address: account.getAddress(),
