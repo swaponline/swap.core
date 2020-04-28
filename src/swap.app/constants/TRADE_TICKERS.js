3d8af4bd40c38ae84b92c0df2a4e0422d4fe29ea
5053s <51257210+5053s@users.noreply.github.com>
2020-04-28 00:06:21 -0600

    update to MASTER

--- src/swap.app/constants/TRADE_TICKERS.js
+++ src/swap.app/constants/TRADE_TICKERS.js
@@ -2,16 +2,7 @@ import { NATIVE, ETH_TOKENS } from './COINS'
 
 export default [
   'ETH-BTC',
-  'EOS-BTC',
-  'LTC-BTC',
-  'ETH-BCH',
-  'ETH-LTC',
-  'SUM-LTC',
-  'BTC-SUM',
-  'SUM-BTC',
-  'SUM-ETH',
-  'QTUM-BTC',
-  
+  'ETH-SUM',
 
   ...Object.values(ETH_TOKENS).map(token => `${token}-BTC`),
   // ...Object.values(ETH_TOKENS).map(token => `${token}-USDT`),
