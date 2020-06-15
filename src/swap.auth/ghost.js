0bf212872aba7d530e94849c71dacfe35495a28d
JoaoCampos89 <jcampos893@gmail.com>
2020-06-15 17:58:28 -0300

    change name on storage key

--- src/swap.auth/ghost.js
+++ src/swap.auth/ghost.js
@@ -4,7 +4,7 @@ import SwapApp from 'swap.app'
 const login = (_privateKey, app) => {
   SwapApp.required(app)
 
-  const storageKey = `${app.network}:sum:privateKey`
+  const storageKey = `${app.network}:ghost:privateKey`
   let privateKey = _privateKey || app.env.storage.getItem(storageKey)
   let account
 
